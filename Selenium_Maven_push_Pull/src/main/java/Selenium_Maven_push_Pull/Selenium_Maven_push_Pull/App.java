b26fbcc88f6d12e1909a7829fea8d4fdafe1d661
chetanasai05 <155408824+chetanasai05@users.noreply.github.com>
2024-01-04 08:24:40 +0530

    added_project

---
+++ Selenium_Maven_push_Pull/src/main/java/Selenium_Maven_push_Pull/Selenium_Maven_push_Pull/App.java
@@ -0,0 +1,13 @@
+package Selenium_Maven_push_Pull.Selenium_Maven_push_Pull;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
